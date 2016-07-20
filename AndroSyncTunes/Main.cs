091536c1697852e14a7f798df6a3bf9588b3ed79
gabry3795 <gabry.gabry@hotmail.it>
2016-07-20 14:06:10 +0200

    Init commit. Working assembler post-build hook for PortableDevice.dll interop

---
+++ AndroSyncTunes/Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using PortableDeviceApiLib;
+using System.Diagnostics;
+
+namespace AndroSyncTunes {
+    public partial class Main : Form {
+        public Main() {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e) {
+            PortableDeviceManager deviceManager = new PortableDeviceManager();
+            deviceManager.RefreshDeviceList();
+            uint cDevices = 1;
+            deviceManager.GetDevices(null, ref cDevices);
+            if (cDevices > 0) {
+                string[] deviceIDs = new string[cDevices];
+                deviceManager.GetDevices(deviceIDs, ref cDevices);
+
+                for (int ndxDevices = 0; ndxDevices < cDevices; ndxDevices++) {
+                    Console.WriteLine("Device[{0}]: {1}",
+                            ndxDevices + 1, deviceIDs[ndxDevices]);
+                }
+            } else {
+                Console.WriteLine("No WPD devices are present!");
+            }
+        }
+
+    }
+}
