80406ea035d4cfcb16c7ec412c8e903f86a2fd19
gabry3795 <gabry.gabry@hotmail.it>
2016-10-23 21:00:46 +0200

    Working simple form listing mtp devices

--- AndroSyncTunes/Main.Designer.cs
+++ AndroSyncTunes/Main.Designer.cs
@@ -25,21 +25,60 @@ namespace AndroSyncTunes {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.device_list_combobox = new System.Windows.Forms.ComboBox();
+            this.select_device_label = new System.Windows.Forms.Label();
+            this.refresh_devices_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // device_list_combobox
+            // 
+            this.device_list_combobox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.device_list_combobox.FormattingEnabled = true;
+            this.device_list_combobox.Location = new System.Drawing.Point(343, 6);
+            this.device_list_combobox.Name = "device_list_combobox";
+            this.device_list_combobox.Size = new System.Drawing.Size(133, 21);
+            this.device_list_combobox.TabIndex = 0;
+            // 
+            // select_device_label
+            // 
+            this.select_device_label.AutoSize = true;
+            this.select_device_label.Location = new System.Drawing.Point(12, 9);
+            this.select_device_label.Name = "select_device_label";
+            this.select_device_label.Size = new System.Drawing.Size(114, 13);
+            this.select_device_label.TabIndex = 1;
+            this.select_device_label.Text = "Select device to Sync:";
+            // 
+            // refresh_devices_button
+            // 
+            this.refresh_devices_button.Location = new System.Drawing.Point(262, 5);
+            this.refresh_devices_button.Name = "refresh_devices_button";
+            this.refresh_devices_button.Size = new System.Drawing.Size(75, 23);
+            this.refresh_devices_button.TabIndex = 2;
+            this.refresh_devices_button.Text = "Refresh";
+            this.refresh_devices_button.UseVisualStyleBackColor = true;
+            this.refresh_devices_button.Click += new System.EventHandler(this.refresh_devices_button_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(488, 245);
+            this.Controls.Add(this.refresh_devices_button);
+            this.Controls.Add(this.select_device_label);
+            this.Controls.Add(this.device_list_combobox);
             this.Name = "Main";
             this.Text = "Main";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox device_list_combobox;
+        private System.Windows.Forms.Label select_device_label;
+        private System.Windows.Forms.Button refresh_devices_button;
     }
 }
 
--- AndroSyncTunes/Main.cs
+++ AndroSyncTunes/Main.cs
@@ -6,10 +6,13 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 using PortableDeviceApiLib;
 using PortableDeviceConstants;
+using WindowsPortableDevicesLib.Domain;
+using WindowsPortableDevicesLib;
 using System.Diagnostics;
 
 namespace AndroSyncTunes {
@@ -19,6 +22,61 @@ namespace AndroSyncTunes {
         }
 
         private void Main_Load(object sender, EventArgs e) {
+            /*
+            StandardWindowsPortableDeviceService service = new StandardWindowsPortableDeviceService();
+
+            Console.WriteLine("Available devices:");
+
+            IList<WindowsPortableDevice> devices = service.Devices;
+
+            if (devices.Count == 0) {
+
+                Console.WriteLine("No devices.");
+            } else {
+
+                int index = 0;
+                foreach (WindowsPortableDevice device in devices) {
+
+                    device.Connect();
+                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.WriteLine("{0} {1} {2}", ++index, device.DeviceModel, device.DeviceID);
+                    var folder = device.GetContents();
+                    foreach (var item in folder.Files) {
+                        DisplayObject(item);
+                    }
+                    Console.WriteLine("ID::{0} PersID::{1}", folder.Files.First().Id, folder.Files.First().PersistentId);
+                    device.TransferContentToDevice("C:\\Users\\gabry\\Downloads\\file.txt", folder.Files.First().Id);
+                    Console.WriteLine("-------------------------------------------------------------");
+
+                    device.Disconnect();
+                }
+            }
+            */
+
+
+
+            //Console.Write("Press any key to continue . . . ");
+            //Console.ReadKey(true);
+        }
+
+        public static void DisplayObject(PortableDeviceObject portableDeviceObject) {
+            Console.WriteLine(portableDeviceObject.Name);
+            if (portableDeviceObject is PortableDeviceFolder) {
+                DisplayFolderContents((PortableDeviceFolder)portableDeviceObject);
+            }
+        }
+
+        public static void DisplayFolderContents(PortableDeviceFolder folder) {
+            foreach (var item in folder.Files) {
+                Console.WriteLine(item.Id);
+                if (item is PortableDeviceFolder) {
+                    DisplayFolderContents((PortableDeviceFolder)item);
+                }
+            }
+        }
+
+        private void Main_Load_old(object sender, EventArgs e) {
+            /*
             Console.WriteLine("\n");
             // Create the client
             // Create our client information collection
@@ -64,21 +122,44 @@ namespace AndroSyncTunes {
 
                 device.Open(deviceIDs[device_chosen], pValues);
                 Console.WriteLine("==> Device[{0}]: Connected", device_chosen);
-                // Enumerate content
+                // Get the content and the resource
                 device.Content(out device_contents);
-                PortableDeviceMethods.Enumerate(ref device_contents, "DEVICE", "");
+                //PortableDeviceMethods.Enumerate(ref device_contents, "DEVICE", "");
                 device_contents.Transfer(out device_resources);
+                // Get the file
+                FileStream file = new FileStream(@"C:\Users\gabry\Downloads\file.txt", FileMode.Open);
+                
+
                 // see https://msdn.microsoft.com/en-us/library/dd388994(v=vs.85).aspx
+                
 
             } else {
                 Console.WriteLine("===> No WPD devices are present!");
             }
+            */
             /*WPDInterfacing.TestInterfacing instance = new WPDInterfacing.TestInterfacing();
             instance.enumerateWPDDevices();
     */
         }
 
+        private void refresh_devices_button_Click(object sender, EventArgs e) {
+            device_list_combobox.Items.Clear();
+            StandardWindowsPortableDeviceService devices_service = new StandardWindowsPortableDeviceService();
+            IList<WindowsPortableDevice> devices = devices_service.Devices;
 
+            if (devices.Count == 0) {
 
+            } else {
+
+                int i = 0;
+                foreach (WindowsPortableDevice device in devices) {
+                    device.Connect();
+                    device_list_combobox.Items.Insert(i++, device.DeviceModel);
+                    device.Disconnect();
+                }
+                
+            }
+            device_list_combobox.SelectedIndex = 0;
+        }
     }
 }
