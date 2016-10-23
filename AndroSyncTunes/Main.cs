52c2e2ac945918d4293364c5586b4f4118293a03
gabry3795 <gabry.gabry@hotmail.it>
2016-10-23 16:47:12 +0200

    Added PortableDeviceConstants and switched to .NET 4

--- AndroSyncTunes/Main.cs
+++ AndroSyncTunes/Main.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using PortableDeviceApiLib;
+using PortableDeviceConstants;
 using System.Diagnostics;
 
 namespace AndroSyncTunes {
@@ -18,21 +19,55 @@ namespace AndroSyncTunes {
         }
 
         private void Main_Load(object sender, EventArgs e) {
+            // Create the client
+            // Create our client information collection
+            PortableDeviceApiLib.IPortableDeviceValues pValues =
+                (PortableDeviceApiLib.IPortableDeviceValues)
+                        new PortableDeviceTypesLib.PortableDeviceValues();
+
+            // We have to provide at the least our name, version, revision       
+            pValues.SetStringValue(
+                    ref PortableDevicePKeys.WPD_CLIENT_NAME, "AndroSyncTunes");
+            pValues.SetUnsignedIntegerValue(
+                    ref PortableDevicePKeys.WPD_CLIENT_MAJOR_VERSION, 1);
+            pValues.SetUnsignedIntegerValue(
+                    ref PortableDevicePKeys.WPD_CLIENT_MINOR_VERSION, 0);
+            pValues.SetUnsignedIntegerValue(
+                    ref PortableDevicePKeys.WPD_CLIENT_REVISION, 2);
+
             PortableDeviceManager deviceManager = new PortableDeviceManager();
+
+
+
             deviceManager.RefreshDeviceList();
             uint cDevices = 1;
             deviceManager.GetDevices(null, ref cDevices);
+
             if (cDevices > 0) {
-                string[] deviceIDs = new string[cDevices + 100];
+                string[] deviceIDs = new string[cDevices];
                 deviceManager.GetDevices(deviceIDs, ref cDevices);
+                // Create devices objects
+                IPortableDevice[] devices = new IPortableDevice[cDevices];
+                IPortableDeviceContent[] devices_contents = new IPortableDeviceContent[cDevices];
+                IPortableDeviceResources[] devices_resources = new IPortableDeviceResources[cDevices];
 
                 for (int ndxDevices = 0; ndxDevices < cDevices; ndxDevices++) {
-                    Console.WriteLine("Device[{0}]: {1}", ndxDevices, deviceIDs[ndxDevices]);
+                    devices[ndxDevices] = new PortableDevice();
+                    devices[ndxDevices].Open(deviceIDs[ndxDevices], null);
+                    Console.WriteLine("==> Device[{0}]: {1}", ndxDevices, deviceIDs[ndxDevices]);
+                    devices[ndxDevices].Open(deviceIDs[ndxDevices], pValues);
                 }
+
             } else {
-                Console.WriteLine("No WPD devices are present!");
+                Console.WriteLine("===> No WPD devices are present!");
             }
+
+            /*WPDInterfacing.TestInterfacing instance = new WPDInterfacing.TestInterfacing();
+            instance.enumerateWPDDevices();
+    */
         }
+
+
 
     }
 }
---
+++ WPDInterfacing/WPDInterfacing.h
@@ -0,0 +1,14 @@
+// WPDInterfacing.h
+
+#pragma once
+
+using namespace System;
+
+namespace WPDInterfacing {
+
+    public ref class TestInterfacing {
+        // TODO: Add your methods for this class here.
+    public: void enumerateWPDDevices();
+
+    };
+}
