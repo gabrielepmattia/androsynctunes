b4feba8894da1cd3db54526f473628965d6c5f14
gabry3795 <gabry.gabry@hotmail.it>
2016-10-25 20:47:21 +0200

    Devices.cs test file

---
+++ AndroSyncTunes/Devices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WindowsPortableDevicesLib.Domain;
+using WindowsPortableDevicesLib;
+
+namespace AndroSyncTunes {
+    class Devices {
+        private StandardWindowsPortableDeviceService devices_service;
+        public IList<WindowsPortableDevice> DevicesList { get; }
+        public IList<IList<PortableDeviceObject>> DevicesResourcesList { get; }
+
+        public Devices() {
+            this.devices_service = new StandardWindowsPortableDeviceService();
+            this.DevicesList = devices_service.Devices;
+            this.DevicesResourcesList = new List<IList<PortableDeviceObject>>();
+            if (DevicesList.Count != 0) {
+                int i = 0;
+                foreach (WindowsPortableDevice device in DevicesList) {
+                    device.Connect();
+                    DevicesResourcesList.Add(new List<PortableDeviceObject>());
+                    foreach (PortableDeviceFolder item in device.GetContents().Files) {
+                        DevicesResourcesList[i].Add(item);
+                    }
+                    device.Disconnect();
+                }
+            }
+        }
+
+    }
+}
